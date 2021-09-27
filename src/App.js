79741585f55e2c913a6ff9715530e0ae8799352e
amankriet <amankriet@gmail.com>
2021-09-27 18:28:33 +0530

    react calc without css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useRef} from 'react'
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [result, setResult] = useState("");
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        inputRef.current.focus();
+    });
+
+    function handleClick(e) {
+        switch(e.target.innerText) {
+            case 'Clear': 
+                setResult("");
+                break;
+            case '␈':
+                setResult(result.slice(0,-1));
+                break;
+            case 'Result':
+                try {
+                    setResult(eval(result).toString());
+                } catch (error) {
+                    setResult("Error");
+                }
+                break;
+            default:
+                setResult(result.concat(e.target.innerText));
+                break;
+        }
+    }
+
+    return (
+        <div className="calc-app">
+            <form action="">
+                <input type="text" value={result} ref={inputRef} />
+            </form>
+
+            <div className="keypad">
+                <button onClick={handleClick}>Clear</button>
+                <button onClick={handleClick}>&#9224;</button>
+                <button onClick={handleClick}>+</button>
+                <button onClick={handleClick}>7</button>
+                <button onClick={handleClick}>8</button>
+                <button onClick={handleClick}>9</button>
+                <button onClick={handleClick}>-</button>
+                <button onClick={handleClick}>4</button>
+                <button onClick={handleClick}>5</button>
+                <button onClick={handleClick}>6</button>
+                <button onClick={handleClick}>&times;</button>
+                <button onClick={handleClick}>1</button>
+                <button onClick={handleClick}>2</button>
+                <button onClick={handleClick}>3</button>
+                <button onClick={handleClick}>&divide;</button>
+                <button onClick={handleClick}>0</button>
+                <button onClick={handleClick}>.</button>
+                <button onClick={handleClick}>Result</button>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
