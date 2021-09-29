b81f05a61a44c57353028849ca48aec8ac8e5f57
amankriet <amankriet@gmail.com>
2021-09-29 12:04:52 +0530

    Functions and css updated

--- src/App.js
+++ src/App.js
@@ -1,66 +1,127 @@
-import React, {useState, useEffect, useRef} from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import "./App.css"
 
 function App() {
-    const [result, setResult] = useState("");
+    const [result, setResult] = useState("0");
     const inputRef = useRef(null);
+    var allowedChars = ['.', '+', '-', '/', '*'];
 
     useEffect(() => {
         inputRef.current.focus();
     });
 
     function handleClick(e) {
-        switch(e.target.innerText) {
-            case 'Clear': 
-                setResult("");
+        switch (e.target.innerText) {
+            case 'Clear':
+                clear();
                 break;
             case '␈':
-                setResult(result.slice(0,-1));
+                backspace();
                 break;
             case '÷':
-                setResult(result.concat("/"));
+                divide();
                 break;
             case '×':
-                setResult(result.concat("*"));
+                multiply();
                 break;
             case 'Result':
-                try {
-                    setResult(eval(result).toString());
-                } catch (error) {
-                    setResult("Error");
-                }
+                calcResult();
                 break;
             default:
-                setResult(result.concat(e.target.innerText));
+                console.log(e.target.innerText);
+                inputText(e.target.innerText);
                 break;
+        }
+        inputRef.current.focus()
+    }
+
+    function check(text) {
+        if(!Number.isNaN(Number.parseInt(text))) {
+            return true;
+        } else if(allowedChars.includes(text)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function inputText(text) {
+        if(result==='0' && text==='0') {
+            clear();
+        } else if(result==='0' && text!=='0' && check(text)) {
+            if(allowedChars.includes(text)) {
+                setResult(result.concat(text));
+            } else {
+                setResult(text.toString());
+            }
+        } else if(check(text)) {
+            setResult(result.concat(text));
+        }
+    }
+
+    function calcResult() {
+        try {
+            setResult(eval(result).toString());
+        } catch (error) {
+            setResult("Error");
+        }
+    }
+
+    function clear() {
+        setResult("0");
+    }
+
+    function backspace() {
+        setResult(result.slice(0, -1));
+        if(result.length===1) {
+            clear();
+        }
+    }
+
+    function divide() {
+        setResult(result.concat("/"));
+    }
+
+    function multiply() {
+        setResult(result.concat("*"));;
+    }
+
+    function handleUserInput(e) {
+        switch (e.key) {
+            case 'Enter':
+                calcResult();
+                break;
+            case 'Backspace':
+                backspace();
+                break;
+            default:
+                inputText(e.key);
         }
     }
 
     return (
         <div className="calc-app">
-            <form action="">
-                <input type="text" value={result} ref={inputRef} readOnly />
-            </form>
+            <input type="text" value={result} ref={inputRef} onChange={handleUserInput} onKeyUp={handleUserInput} />
 
             <div className="keypad">
-                <button id="btn-clear" onClick={handleClick}>Clear</button>
-                <button id="btn-backspace" onClick={handleClick}>&#9224;</button>
-                <button onClick={handleClick}>+</button>
+                <button className="highlight" id="btn-clear" onClick={handleClick}>Clear</button>
+                <button className="highlight" id="btn-backspace" onClick={handleClick}>&#9224;</button>
+                <button className="highlight" onClick={handleClick}>+</button>
                 <button onClick={handleClick}>7</button>
                 <button onClick={handleClick}>8</button>
                 <button onClick={handleClick}>9</button>
-                <button onClick={handleClick}>-</button>
+                <button className="highlight" onClick={handleClick}>-</button>
                 <button onClick={handleClick}>4</button>
                 <button onClick={handleClick}>5</button>
                 <button onClick={handleClick}>6</button>
-                <button onClick={handleClick}>&times;</button>
+                <button className="highlight" onClick={handleClick}>&times;</button>
                 <button onClick={handleClick}>1</button>
                 <button onClick={handleClick}>2</button>
                 <button onClick={handleClick}>3</button>
-                <button onClick={handleClick}>&divide;</button>
+                <button className="highlight" onClick={handleClick}>&divide;</button>
                 <button onClick={handleClick}>0</button>
                 <button onClick={handleClick}>.</button>
-                <button id="btn-result" onClick={handleClick}>Result</button>
+                <button className="highlight" id="btn-result" onClick={handleClick}>Result</button>
             </div>
         </div>
     )
